25696da71e8bbd431ce9c014ece54ba0bbfff43b
Marjorie <147122517+MarjorieAltura@users.noreply.github.com>
2023-10-17 15:19:58 +0800

    Add files via upload

---
+++ Altura/act7.js
@@ -0,0 +1,8 @@
+let p = 'p@sswOrd@2023';
+
+if(p.length >= p.includes("p@ssword")){
+    console.log("valid password");
+}
+else{
+    console.log("Invalid password");
+}
---
+++ Altura/act8.js
@@ -0,0 +1,17 @@
+const favSingers =[ 'Taylor Swift', 'Selena Gomez', 'Moira Dela Torre' ];
+
+console.log ( favSingers [0]);
+console.log ( favSingers [1]);
+console.log ( favSingers [2]);
+
+
+const favNumbers = [ '9', '16', '21', '11'];
+
+console.log ( favNumbers [0]);
+console.log ( favNumbers [1]);
+console.log ( favNumbers [2]);
+console.log ( favNumbers [3]);
+
+const mixedArr = [ " string ", ["otherarray"], 123, true];
+
+console.log ( favSingers [0], favSingers [1], favSingers[2], favNumbers [1], mixedArr [3]);
